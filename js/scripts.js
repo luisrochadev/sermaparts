f4095dfd0570eba76fa23fbfe909c86e9b9c88f5
LUIS ROCHA <luisrocha1993@gmail.com>
2016-12-11 18:29:01 -0800

    redesign v1

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
-(function($){
-  $(function(){
+$(document).ready(function() {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-$(document).ready(function() {
     $(".dropdown-button").dropdown();
     
+    $("#customer-carousel").owlCarousel();
+
+    $(window).scroll(function () {
+    var topDivHeight = $("#par-one").height();
+    var viewPortSize = $(window).height();
+
+    var triggerAt = 1250;
+    var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
+
+    if ($(window).scrollTop() >= triggerHeight) {
+        $('#par-one').addClass('animated fadeIn');
+    }
 });
+
+    $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top -64
+    }, 500);
+    return false;
+});
+
+    
+
+  }); // end of document ready
+ // end of jQuery name space
--- scripts.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $(".dropdown-button").dropdown();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
